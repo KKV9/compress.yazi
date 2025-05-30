84129fc2aba3258e0d69d6c4b4d69fdc18458974
KKV9 <KKV9@github.com>
2025-05-30 21:19:18 +0100

    Bugfixes and Big refactor

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A Yazi plugin that compresses selected files to an archive. Supporting yazi v25.
 
 ## Supported file types
 
-| Extention     | Unix Command  | Windows Command |
+| Extension     | Unix Command  | Windows Command |
 | ------------- | ------------- | --------------- |
 | .zip          | zip -r        | 7z a -tzip      |
 | .7z           | 7z a          | 7z a            |
@@ -35,23 +35,23 @@ ya pkg add KKV9/compress
 
 ```toml
 [[mgr.prepend_keymap]]
-on   = [ "c", "a" ]
+on   = [ "c", "a", "a" ]
 run  = "plugin compress"
 desc = "Archive selected files"
 [[mgr.prepend_keymap]]
-on   = [ "c", "p" ]
+on   = [ "c", "a", "p" ]
 run  = "plugin compress -p"
 desc = "Archive selected files (password)"
 [[mgr.prepend_keymap]]
-on   = [ "c", "h" ]
+on   = [ "c", "a", "h" ]
 run  = "plugin compress -ph"
 desc = "Archive selected files (password+header)"
 [[mgr.prepend_keymap]]
-on   = [ "c", "l" ]
+on   = [ "c", "a", "l" ]
 run  = "plugin compress -l"
 desc = "Archive selected files (compression level)"
 [[mgr.prepend_keymap]]
-on   = [ "c", "u" ]
+on   = [ "c", "a", "u" ]
 run  = "plugin compress -phl"
 desc = "Archive selected files (password+header+level)"
 ```
@@ -59,8 +59,16 @@ desc = "Archive selected files (password+header+level)"
 ## Usage
 
  - Select files or folders to add, then press `c` `a` to create a new archive.
+ - Press `a` again for a standard archive. 
+ -  OR Press `p` for a password protected archive (7z and zip only). 
+ -  OR Press `h` to also encrypt the header (7zip only)
+ -  OR Press `l` to select level of compression (all algorithims except zstd).
+ -  OR Press `u` to get everything.
  - Type a name for the new file. 
- - The file extention must match one of the supported filetype extentions.
+ - Enter the password if selected.
+ - Enter the compression level (0-9) if selected. 0 works for zip and 7z only.
+ - Overwrite prompt will appear if filename matches another file. Type y to overwrite or type n or press enter to cancel.
+ - The file extension must match one of the supported filetype extensions.
  - The desired archive/compression command must be installed on your system.
 
 ## Flags
@@ -69,4 +77,5 @@ desc = "Archive selected files (password+header+level)"
  - -p allows you to set a password (7z and zip only)
  - -h allows you to encrypt header (7z only)
  - -l allows you to set a compression level (0 - 9) -- 0 = Store, 9 = Best compression
- - Compression level currently works for all compression algorithims except zstd
+
+**NOTE:** Compression level currently works for all compression algorithims except zstd.
--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 -- Check for windows
 local is_windows = ya.target_family() == "windows"
--- Define flags
-local is_password, is_encrypted, is_level = false, false, false
+-- Define flags and strings
+local is_password, is_encrypted, is_level, cmd_password, cmd_level = false, false, false, "", ""
 
--- Send error notification
+-- Function to send notifications
 local function notify_error(message, urgency)
 	ya.notify({
 		title = "Archive",
@@ -13,7 +13,64 @@ local function notify_error(message, urgency)
 	})
 end
 
--- Make table of selected or hovered: path = filenames
+-- Function to check if command is available
+local function is_command_available(cmd)
+	local stat_cmd
+	if is_windows then
+		stat_cmd = string.format("where %s > nul 2>&1", cmd)
+	else
+		stat_cmd = string.format("command -v %s >/dev/null 2>&1", cmd)
+	end
+	local cmd_exists = os.execute(stat_cmd)
+	if cmd_exists then
+		return true
+	else
+		return false
+	end
+end
+
+-- Function to change command arrays --> string -- Use first command available or first command
+local function find_binary(cmd_list)
+	for _, cmd in ipairs(cmd_list) do
+		if is_command_available(cmd) then
+			return cmd
+		end
+	end
+	return cmd_list[1] -- Return first command as fallback
+end
+
+-- Function to check if a file exists
+local function file_exists(name)
+	local f = io.open(name, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
+end
+
+-- Function to append filename to it's parent directory url
+local function combine_url(path, file)
+	path, file = Url(path), Url(file)
+	return tostring(path:join(file))
+end
+
+-- Function to display errors if a command goes wrong
+local function command_warning(command, status, error)
+		if not status or not status.success then
+			notify_error(
+			string.format(
+				"%s with selected files failed, exit code %s",
+				command,
+				status and status.code or error
+			),
+			"error"
+		)
+	end
+end
+
+-- Function to make a table of selected or hovered files: path = filenames
 local selected_or_hovered = ya.sync(function()
 	local tab, paths, names, path_fnames = cx.active, {}, {}, {}
 	for _, u in pairs(tab.selected) do
@@ -33,67 +90,70 @@ local selected_or_hovered = ya.sync(function()
 	return path_fnames, tostring(tab.current.cwd)
 end)
 
--- Check if archive command is available
-local function is_command_available(cmd)
-	local stat_cmd
-
-	if is_windows then
-		stat_cmd = string.format("where %s > nul 2>&1", cmd)
-	else
-		stat_cmd = string.format("command -v %s >/dev/null 2>&1", cmd)
-	end
-
-	local cmd_exists = os.execute(stat_cmd)
-	if cmd_exists then
-		return true
-	else
-		return false
-	end
-end
-
--- Archive command list --> string
-local function find_binary(cmd_list)
-	for _, cmd in ipairs(cmd_list) do
-		if is_command_available(cmd) then
-			return cmd
-		end
-	end
-	return cmd_list[1] -- Return first command as fallback
-end
-
--- Check if file exists
-local function file_exists(name)
-	local f = io.open(name, "r")
-	if f ~= nil then
-		io.close(f)
-		return true
-	else
-		return false
-	end
-end
-
--- Append filename to it's parent directory
-local function combine_url(path, file)
-	path, file = Url(path), Url(file)
-	return tostring(path:join(file))
-end
+-- Table of archive commands
+local archive_commands = {
+	["%.zip$"] = {
+		unix = { command = "zip", args = { "-r" }, level_arg = "-", passwordable = true },
+		windows = { command = "7z", args = { "a", "-tzip" }, level_arg = "-mx=", passwordable = true },
+	},
+	["%.7z$"] = {
+		unix = { command = { "7z", "7zz" }, args = { "a" }, level_arg = "-mx=", header_arg = "-mhe=on", passwordable = true },
+		windows = { command = "7z", args = { "a" }, level_arg = "-mx=", header_arg = "-mhe=on", passwordable = true },
+	},
+	["%.tar.gz$"] = {
+		unix = { command = "tar", args = { "rpf" }, level_arg = "-", compress = "gzip" },
+		windows = {
+			command = "tar",
+			args = { "rpf" },
+			level_arg = "-mx=",
+			compress = "7z",
+			compress_args = { "a", "-tgzip", "-sdel" },
+		},
+	},
+	["%.tar.xz$"] = {
+		unix = { command = "tar", args = { "rpf" }, level_arg = "-", compress = "xz" },
+		windows = {
+			command = "tar",
+			args = { "rpf" },
+			level_arg = "-mx=",
+			compress = "7z",
+			compress_args = { "a", "-txz", "-sdel" },
+		},
+	},
+	["%.tar.bz2$"] = {
+		unix = { command = "tar", args = { "rpf" }, level_arg = "-", compress = "bzip2" },
+		windows = {
+			command = "tar",
+			args = { "rpf" },
+			level_arg = "-mx=",
+			compress = "7z",
+			compress_args = { "a", "-tbzip2", "-sdel" },
+		},
+	},
+	["%.tar.zst$"] = {
+		unix = { command = "tar", args = { "rpf" }, compress = "zstd", compress_args = { "--rm" } },
+		windows = { command = "tar", args = { "rpf" }, compress = "zstd", compress_args = { "--rm" } },
+	},
+	["%.tar$"] = {
+		unix = { command = "tar", args = { "rpf" } },
+		windows = { command = "tar", args = { "rpf" } },
+	},
+}
 
 return {
 	entry = function(_, job)
-		-- Check if password is needed. Search for char in arguments string
-		if job.args[1] ~= nil then
-			is_password = string.find(tostring(job.args[1]), "p") ~= nil
-			is_encrypted = string.find(tostring(job.args[1]), "h") ~= nil
-			is_level = string.find(tostring(job.args[1]), "l") ~= nil
+		-- Check if password is needed. Search for char in the entire arguments array as a single string
+		if job.args ~= nil then
+			local args_string = table.concat(job.args, "")
+			is_password = string.find(args_string, "p") ~= nil
+			is_encrypted = string.find(args_string, "h") ~= nil
+			is_level = string.find(args_string, "l") ~= nil
 		end
-
 		-- Exit visual mode
 		ya.manager_emit("escape", { visual = true })
-
 		-- Define file table and output_dir (pwd)
 		local path_fnames, output_dir = selected_or_hovered()
-
-		-- Get input
+		-- Get archive filename
 		local output_name, event = ya.input({
 			title = "Create archive:",
 			position = { "top-center", y = 3, w = 40 },
@@ -102,78 +162,24 @@ return {
 			return
 		end
 
-		-- Use appropriate archive command
-		local archive_commands = {
-			["%.zip$"] = {
-				unix = { command = "zip", args = { "-r" }, level_arg = "-", passwordable = true },
-				windows = { command = "7z", args = { "a", "-tzip" }, level_arg = "-mx=", passwordable = true },
-			},
-			["%.7z$"] = {
-				unix = { command = { "7z", "7zz" }, args = { "a" }, level_arg = "-mx=", header_arg = "-mhe=on", passwordable = true },
-				windows = { command = "7z", args = { "a" }, level_arg = "-mx=", header_arg = "-mhe=on", passwordable = true },
-			},
-			["%.tar.gz$"] = {
-				unix = { command = "tar", args = { "rpf" }, level_arg = "-", compress = "gzip" },
-				windows = {
-					command = "tar",
-					args = { "rpf" },
-					level_arg = "-mx=",
-					compress = "7z",
-					compress_args = { "a", "-tgzip", "-sdel", output_name },
-				},
-			},
-			["%.tar.xz$"] = {
-				unix = { command = "tar", args = { "rpf" }, level_arg = "-", compress = "xz" },
-				windows = {
-					command = "tar",
-					args = { "rpf" },
-					level_arg = "-mx=",
-					compress = "7z",
-					compress_args = { "a", "-txz", "-sdel", output_name },
-				},
-			},
-			["%.tar.bz2$"] = {
-				unix = { command = "tar", args = { "rpf" }, level_arg = "-", compress = "bzip2" },
-				windows = {
-					command = "tar",
-					args = { "rpf" },
-					level_arg = "-mx=",
-					compress = "7z",
-					compress_args = { "a", "-tbzip2", "-sdel", output_name },
-				},
-			},
-			["%.tar.zst$"] = {
-				unix = { command = "tar", args = { "rpf" }, compress = "zstd", compress_args = { "--rm" } },
-				windows = { command = "tar", args = { "rpf" }, compress = "zstd", compress_args = { "--rm" } },
-			},
-			["%.tar$"] = {
-				unix = { command = "tar", args = { "rpf" } },
-				windows = { command = "tar", args = { "rpf" } },
-			},
-		}
-
 		-- Match user input to archive command
 		local archive_cmd, archive_args, archive_compress, archive_level_arg, archive_header_arg, archive_passwordable, archive_compress_args
 		for pattern, cmd_pair in pairs(archive_commands) do
 			if output_name:match(pattern) then
-				if is_windows then
-					archive_cmd = cmd_pair.windows.command
-					archive_args = cmd_pair.windows.args
-					archive_compress = cmd_pair.windows.compress or ""
-					archive_level_arg = is_level and cmd_pair.windows.level_arg or ""
-					archive_header_arg = is_encrypted and cmd_pair.windows.header_arg or ""
-					archive_passwordable = cmd_pair.windows.passwordable or false
-					archive_compress_args = cmd_pair.windows.compress_args or {}
-				else
-					archive_cmd = cmd_pair.unix.command
-					archive_args = cmd_pair.unix.args
-					archive_compress = cmd_pair.unix.compress or ""
-					archive_level_arg = is_level and cmd_pair.unix.level_arg or ""
-					archive_header_arg = is_encrypted and cmd_pair.unix.header_arg or ""
-					archive_passwordable = cmd_pair.unix.passwordable or false
-					archive_compress_args = cmd_pair.unix.compress_args or {}
-				end
+				local os_cmd = is_windows and cmd_pair.windows or cmd_pair.unix
+				archive_cmd = os_cmd.command
+				archive_args = os_cmd.args
+				archive_compress = os_cmd.compress or ""
+				archive_level_arg = is_level and os_cmd.level_arg or ""
+				archive_header_arg = is_encrypted and os_cmd.header_arg or ""
+				archive_passwordable = os_cmd.passwordable or false
+				archive_compress_args = os_cmd.compress_args or {}
 			end
+		end
+
+		-- 7z needs to know tar archive to delete after compression
+		if archive_compress_args[3] == "-sdel" then
+			table.insert(archive_compress_args, output_name)
 		end
 
 		-- Check if archive command has multiple names
@@ -197,6 +203,48 @@ return {
 		if archive_compress ~= "" and not is_command_available(archive_compress) then
 			notify_error(string.format("%s compression not available", archive_compress), "error")
 			return
+		end
+
+		-- Add password arg if selected
+		if archive_passwordable and is_password then
+			local output_password, event = ya.input({
+				title = "Enter password:",
+				obscure = true,
+				position = { "top-center", y = 3, w = 40 },
+			})
+			if event ~= 1 then
+				return
+			end
+			if output_password ~= "" then
+				cmd_password = "-P" .. output_password
+				table.insert(archive_args, cmd_password)
+			end
+		end
+
+		-- Add header arg if selected
+		if is_encrypted and archive_header_arg ~= "" then
+			table.insert(archive_args, archive_header_arg)
+		end
+
+		-- Add level arg if selected
+		if archive_level_arg ~= "" and is_level then
+			local output_level, event = ya.input({
+				title = "Enter compression level (0 - 9):",
+				position = { "top-center", y = 3, w = 40 },
+			})
+			if event ~= 1 then
+				return
+			end
+			-- Make sure this string is a single digit. False if using tar & compression level is 0 (defeats the purpose of using compression).
+			if output_level ~= "" and tonumber(output_level) ~= nil and string.len(output_level) == 1 and not ( archive_cmd == "tar" and output_level == "0" ) then
+				cmd_level = archive_level_arg .. output_level
+			end
+			-- Decide if level will be used for compression command or archive command
+			if archive_compress == "" then
+				table.insert(archive_args, cmd_level)
+			else
+				table.insert(archive_compress_args, cmd_level)
+			end
 		end
 
 		-- If file exists show overwrite prompt
@@ -226,83 +274,18 @@ return {
 			end
 		end
 
-		local archive_additional_args = {}
-		local compress_additional_args = {}
-
-		-- Get password if selected
-		local cmd_password = ""
-		if archive_passwordable and is_password then
-			local output_password, event = ya.input({
-				title = "Enter password:",
-				obscure = true,
-				position = { "top-center", y = 3, w = 40 },
-			})
-			if event ~= 1 then
-				return
-			end
-			if output_password ~= "" then
-				cmd_password = "-P" .. output_password
-				table.insert(archive_additional_args, cmd_password)
-			end
-		end
-
-		if is_encrypted and archive_header_arg ~= "" then
-			table.insert(archive_additional_args, archive_header_arg)
-		end
-
-		-- Get level if selected
-		local cmd_level = ""
-		if archive_level_arg ~= "" and is_level then
-			local output_level, event = ya.input({
-				title = "Enter compression level (0 - 9):",
-				position = { "top-center", y = 3, w = 40 },
-			})
-			if event ~= 1 then
-				return
-			end
-			-- Make sure this string is a single digit. False if using tar & compression level is 0 (defeats the purpose of using compression).
-			if output_level ~= "" and tonumber(output_level) ~= nil and string.len(output_level) == 1 and not ( archive_cmd == "tar" and output_level == "0" ) then
-				cmd_level = archive_level_arg .. output_level
-			end
-			-- Decide if level will be used for compression command or archive command
-			if archive_compress == "" then
-				table.insert(archive_additional_args, cmd_level)
-			else
-				table.insert(compress_additional_args, cmd_level)
-			end
-		end
-
 		-- Add to output archive in each path, their respective files
-		for path, names in pairs(path_fnames) do
+		for filepath, filenames in pairs(path_fnames) do
 			local archive_status, archive_err = 
-			Command(archive_cmd):arg(archive_args):arg(archive_additional_args):arg(output_url):arg(names):cwd(path):spawn():wait()
-			if not archive_status or not archive_status.success then
-				notify_error(
-					string.format(
-						"%s with selected files failed, exit code %s",
-						archive_cmd,
-						archive_status and archive_status.code or archive_err
-					),
-					"error"
-				)
-			end
+			Command(archive_cmd):arg(archive_args):arg(output_url):arg(filenames):cwd(filepath):spawn():wait()
+			command_warning(archive_cmd, archive_status, archive_err)
 		end
 
 		-- Use compress command if needed
 		if archive_compress ~= "" then
 			local compress_status, compress_err =
-				Command(archive_compress):arg(archive_compress_args):arg(compress_additional_args):arg(output_name):cwd(output_dir):spawn():wait()
-					if not compress_status or not compress_status.success then
-						notify_error(
-						string.format(
-							"%s with %s failed, exit code %s",
-							archive_compress,
-							output_name,
-							compress_status and compress_status.code or compress_err
-						),
-						"error"
-					)
-			end
+			Command(archive_compress):arg(archive_compress_args):arg(output_name):cwd(output_dir):spawn():wait()
+			command_warning(archive_compress, compress_status, compress_err)
 		end
 	end,
 }
